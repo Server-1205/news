3e723b02011422fe55923939cf2903f6ba6bc0e0
Server <server1205@gmail.com>
2025-01-07 20:50:59 +0500

    git init

---
+++ src/components/NewsItem/NewsItem.jsx
@@ -0,0 +1,20 @@
+﻿/* eslint-disable react/prop-types */
+import { formatTimeAgo } from '../../helpers/formatTimeAgo';
+import styles from './styles.module.css';
+
+const NewsItem = ({ item }) => {
+  return (
+    <li className={styles.item}>
+      <div className={styles.wrapper}>
+        <img src={item.image} alt='news' />
+      </div>
+      <div className={styles.info}>
+        <h3 className={styles.title}>{item.title}</h3>
+
+        <p>{formatTimeAgo(item.published)}</p>
+      </div>
+    </li>
+  );
+};
+
+export default NewsItem;
